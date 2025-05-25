83a6dcbfb07c0059aa6dd97efa97cce864cb61f3
Ahmed Hassan <7moody63600@gmail.com>
2025-05-25 13:48:28 +0300

    Update WhyILoveYou.js

--- src/components/WhyILoveYou.js
+++ src/components/WhyILoveYou.js
@@ -7,7 +7,8 @@ const WhyILoveYou = () => {
             <ul>
                 <li>Your kindness and warmth that brighten my days.</li>
                 <li>The way you make me smile even in tough times.</li>
-                <li>Our shared dreams, laughter, and endless adventures.</li>
+                <li>Our shared talks, laughter, and endless adventures.</li>
+                <li>Even though you are the farthest person, you are the closest to my heart.</li>
             </ul>
         </section>
     );
