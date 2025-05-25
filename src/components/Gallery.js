fc864e9cebc230e38df5dd303c639875e762bbd6
Ahmed Hassan <7moody63600@gmail.com>
2025-05-25 13:30:50 +0300

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,6 +12,14 @@ const images = [
     '/8.jpg',
     '/9.jpg',
     '/10.jpg',
+    '/11.jpg',
+    '/12.jpg',
+    '/13.jpg',
+    '/14.jpg',
+    '/15.jpg',
+    '/16.jpg',
+    '/17.jpg',
+    '/18.jpg',
 ];
 
 const Gallery = () => {
